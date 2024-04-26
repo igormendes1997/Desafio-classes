140d175800287f8ea2888b66bec8cf74f11e1f39
igormendes1997 <igor19_@outlook.com>
2024-04-26 08:37:58 -0300

    Definir uma classe

---
+++ index.js
@@ -0,0 +1,10 @@
+// Definir uma classe
+class Heroi{
+    constructor( nome, idade , tipo){
+        this.nome = Igor;
+        this.idade = 26;
+        this.tipo = ninja;
+        
+    }
+    
+}
